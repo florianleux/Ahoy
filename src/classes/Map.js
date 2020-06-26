0318c8450d5481dfbc85297e451c4c5dbfbb93ff
Florian Leux <florian.leux@gmail.com>
2020-06-26 11:29:42 +0200

    Ajout du rotate sur le placement
    Ajout de lodash et utilisation de _.throttle

--- src/classes/Map.js
+++ src/classes/Map.js
@@ -20,16 +20,32 @@ export class Map {
         posY = Number(square.dataset.y) - 1,
         boatSize = Number(selectedBoat.size) - 1,
         half = Math.floor(boatSize / 2),
-        remain = boatSize % 2,
-        min = posX - half >= 0 ? posX - half : 0,
-        max = posX + half + remain <= 9 ? posX + half + remain : 9;
+        remain = boatSize % 2;
 
-      for (let i = min; i <= posX; i++) {
-        this.hoverMap[posY].splice(i, 1, true);
-      }
+      console.log(selectedBoat.horizontal);
 
-      for (let i = max; i > posX; i--) {
-        this.hoverMap[posY].splice(i, 1, true);
+      if (selectedBoat.horizontal) {
+        var min = posX - half >= 0 ? posX - half : 0,
+          max = posX + half + remain <= 9 ? posX + half + remain : 9;
+
+        for (let i = min; i <= posX; i++) {
+          this.hoverMap[posY].splice(i, 1, true);
+        }
+
+        for (let i = max; i > posX; i--) {
+          this.hoverMap[posY].splice(i, 1, true);
+        }
+      } else {
+        (min = posY - half >= 0 ? posY - half : 0),
+          (max = posY + half + remain <= 9 ? posY + half + remain : 9);
+
+        for (let i = min; i <= posX; i++) {
+          this.hoverMap[i].splice(posX, 1, true);
+        }
+
+        for (let i = max; i > posX; i--) {
+          this.hoverMap[i].splice(posX, 1, true);
+        }
       }
     }
   }
@@ -43,16 +59,32 @@ export class Map {
         posY = Number(square.dataset.y) - 1,
         boatSize = Number(selectedBoat.size) - 1,
         half = Math.floor(boatSize / 2),
-        remain = boatSize % 2,
-        min = posX - half >= 0 ? posX - half : 0,
-        max = posX + half + remain <= 9 ? posX + half + remain : 9;
+        remain = boatSize % 2;
 
-      for (let i = min; i <= posX; i++) {
-        this.boatMap[posY].splice(i, 1, true);
-      }
+      console.log(selectedBoat.horizontal);
 
-      for (let i = max; i > posX; i--) {
-        this.boatMap[posY].splice(i, 1, true);
+      if (selectedBoat.horizontal) {
+        var min = posX - half >= 0 ? posX - half : 0,
+          max = posX + half + remain <= 9 ? posX + half + remain : 9;
+
+        for (let i = min; i <= posX; i++) {
+          this.boatMap[posY].splice(i, 1, true);
+        }
+
+        for (let i = max; i > posX; i--) {
+          this.boatMap[posY].splice(i, 1, true);
+        }
+      } else {
+        (min = posY - half >= 0 ? posY - half : 0),
+          (max = posY + half + remain <= 9 ? posY + half + remain : 9);
+
+        for (let i = min; i <= posX; i++) {
+          this.boatMap[i].splice(posX, 1, true);
+        }
+
+        for (let i = max; i > posX; i--) {
+          this.boatMap[i].splice(posX, 1, true);
+        }
       }
 
       selectedBoat.unselect();
--- src/components/Placement/Map.vue
+++ src/components/Placement/Map.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-row id="map" v-if="map.hoverMap[9]">
+  <v-row id="map" v-if="map.hoverMap[9]" @wheel.prevent="throttledRotateBoat">
     <div class="canvas">
       <div class="line" v-for="n in 10" :key="n">
         <div
@@ -21,21 +21,39 @@
 </template>
 
 <script>
+import _ from "lodash";
+
 export default {
   name: "MapVue",
   data: function() {
     return {
       game: this.$game,
-      map: this.$game.player.map
+      fleet: this.$game.player.fleet,
+      map: this.$game.player.map,
+      target: null
     };
   },
   methods: {
     hoverSquare: function(event) {
+      this.target = event.target;
       this.map.hoverSquare(event.target, this.$game.player.fleet);
     },
     clickSquare: function(event) {
       this.map.placeBoat(event.target, this.$game.player.fleet);
+    },
+    rotateBoat: function() {
+      if (this.fleet.selectedBoat) {
+        this.fleet.selectedBoat.horizontal = !this.fleet.selectedBoat
+          .horizontal;
+        this.map.hoverSquare(this.target, this.$game.player.fleet);
+      }
     }
+  },
+  created() {
+    this.throttledRotateBoat = _.throttle(this.rotateBoat, 200, {
+      leading: true,
+      trailing: false
+    });
   }
 };
 </script>
